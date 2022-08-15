f5041943951c19aa0c8aa078e8ebbf40b224d98c
JonneBR <felipe_spinassi@hotmail.com>
2022-08-15 16:54:49 -0300

    updating app

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <>
       <StatusBar
-        barStyle="light-content"
+        barStyle="dark-content"
         backgroundColor="transparent"
         translucent
       />
